526e4d1d98113cf187c0556e8b07ce3d076c6651
ticak <tijanak1991@hotmail.com>
2019-05-28 15:57:04 +0200

    #PITL-18 GenreTableSeeders, Genre Model, Movie Model, show single movie

--- app/Genre.php
+++ app/Genre.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App;
-
+use App\Movie;
 use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
     public $timestamps = false;
+
+    public function movies(){
+        return $this->belongsToMany(Movie::class);
+    }
 }
--- app/Movie.php
+++ app/Movie.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Genre;
 
 class Movie extends Model
 {
-    //
+    public function genre(){
+        return $this->belongsTo(Genre::class);
+    }
 }
