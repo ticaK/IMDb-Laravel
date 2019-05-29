5fdc8b52ade21e21dd7eadff67ddf8cd2df48427
ticak <tijanak1991@hotmail.com>
2019-05-29 16:53:07 +0200

    #PITL-27 search movies

--- app/Movie.php
+++ app/Movie.php
@@ -10,4 +10,14 @@ class Movie extends Model
     public function genre(){
         return $this->belongsTo(Genre::class);
     }
+    
+    public static function search($searchTerm) {
+        $query = Movie::query();
+        if ($searchTerm) {
+            $query->where('title','like','%'.$searchTerm.'%');  
+        }
+        return response()->json([
+            'movies' => $query->paginate(3)
+        ]);
+    }
 }
